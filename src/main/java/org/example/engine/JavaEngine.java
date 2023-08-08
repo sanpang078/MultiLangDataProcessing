c4c4971a0096974f490f135445958788da11cf40
sanpang <sanpang0309@163.com>
2023-08-08 20:11:06 +0800

    create repository

---
+++ src/main/java/org/example/engine/JavaEngine.java
@@ -0,0 +1,15 @@
+package org.example.engine;
+
+import java.util.List;
+import java.util.Map;
+
+public class JavaEngine {
+    public static List<Map.Entry<String, Long>> groupAndAggregate(
+            List<Map<String, Long>> dataSet,
+            String groupField,
+            String aggregateField,
+            String operator
+    ){
+
+    }
+}
